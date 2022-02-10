5072b03017e4b85a7feba9cf2f9f0613cfe68126
Roman Golovatyi <romangolovatyi@yahoo.com>
2022-02-10 12:47:21 +0200

    Added phone storage service

--- homework/src/model/PhoneStorageService.java
+++ homework/src/model/PhoneStorageService.java
@@ -1,2 +1,11 @@
-package model;public class PhoneStorageService {
+package model;
+
+public class PhoneStorageService {
+    public void receivePhone(Phone phone){
+        System.out.println("Receiving phone (model: " + phone.getModel() + ", color: " + phone.getColor() + "imeiCode: "+ phone.getImeiCode() + "), saving it into DB");
+    }
+
+    public void sellPhone(Phone phone){
+        System.out.println("Selling phone (model: " + phone.getModel() + ", color: " + phone.getColor() + "imeiCode: "+ phone.getImeiCode() + "), delete it into DB.");
+    }
 }
